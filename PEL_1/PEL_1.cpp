ed44c35c31b3ea9a597a278256a773fb6a70fa0e
Filip Viduna <viduna.f@gmail.com>
2021-02-15 14:36:40 +0100

    Přidejte soubory projektu.

---
+++ PEL_1/PEL_1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    uint16_t bez_znamenka, i;
+    int16_t znamenko, j;
+
+    bez_znamenka = i = numeric_limits<uint16_t>::min();
+    znamenko = j = numeric_limits<int16_t>::min();
+
+    do {
+        i++;
+        j++;
+        printf("%d %d, hex: %x %x\n", i, j, i, j);
+    } while (i > bez_znamenka && j > znamenko);
+}
